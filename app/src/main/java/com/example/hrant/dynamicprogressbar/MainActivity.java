776e6ae32c9a01e3cee58ad51cd5b2164c3edf06
Hrant75 <hhro@yahoo.com>
2017-05-03 21:47:22 +0400

    dynamic progress bar

--- app/src/main/java/com/example/hrant/dynamicprogressbar/MainActivity.java
+++ app/src/main/java/com/example/hrant/dynamicprogressbar/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.hrant.dynamicprogressbar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.RadioButton;
@@ -12,7 +16,7 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     ProgressBar progressBar;
     SeekBar seekBar;
     EditText editTextProgres, editTextSeekBar;
@@ -23,9 +27,14 @@ public class MainActivity extends AppCompatActivity {
     Button button_clear_all_radios;
     Switch aSwitch_add10, aSwitch_add20, aSwitch_add30;
 
+    PreferencesHelper preferencesHelper;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
         progressBar = (ProgressBar)findViewById(R.id.progress_bar);
         seekBar = (SeekBar)findViewById(R.id.seek_bar);
         editTextProgres = (EditText)findViewById(R.id.progress_editText);
@@ -44,11 +53,144 @@ public class MainActivity extends AppCompatActivity {
         aSwitch_add10 = (Switch)findViewById(R.id.switch_add10);
         aSwitch_add20 = (Switch)findViewById(R.id.switch_add20);
         aSwitch_add30 = (Switch)findViewById(R.id.switch_add30);
-        PreferencesHelper preferencesHelper = PreferencesHelper.getInstance(this);
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        final PreferencesHelper preferencesHelper = PreferencesHelper.getInstance(this);
 
+        progressBar.setProgress(preferencesHelper.getProgressBarValue());
+        seekBar.setProgress(preferencesHelper.getSeekBarValue());
+//        editTextProgres.setText(preferencesHelper.getProgressBarMax());
+//        editTextSeekBar.setText(preferencesHelper.getSeekBarMax());
+        textViewProgress.setText("ProgressBar: " + progressBar.getProgress());
+        textViewSeek.setText("SeekBar: " + seekBar.getProgress());
+        checkBoxToggles.setChecked(preferencesHelper.getCheckBoxState());
+        toggleButton2x.setChecked(preferencesHelper.getToggle2xState());
+        toggleButton4x.setChecked(preferencesHelper.getToggle4xState());
+        toggleButton6x.setChecked(preferencesHelper.getToggle6xState());
+        toggleButton8x.setChecked(preferencesHelper.getToggle8xState());
+        radioButton_div2.setChecked(preferencesHelper.getRadioButtonDiv2State());
+        radioButton_div4.setChecked(preferencesHelper.getRadioButtonDiv4State());
+        radioButton_div8.setChecked(preferencesHelper.getRadioButtonDiv8State());
+        aSwitch_add10.setChecked(preferencesHelper.getSwitchAdd10State());
+        aSwitch_add20.setChecked(preferencesHelper.getSwitchAdd20State());
+        aSwitch_add30.setChecked(preferencesHelper.getSwitchAdd30State());
 
+        checkBoxToggles.setOnClickListener(this);
+        toggleButton2x.setOnClickListener(this);
+        toggleButton4x.setOnClickListener(this);
+        toggleButton6x.setOnClickListener(this);
+        toggleButton8x.setOnClickListener(this);
+        radioButton_div2.setOnClickListener(this);
+        radioButton_div4.setOnClickListener(this);
+        radioButton_div8.setOnClickListener(this);
+        aSwitch_add10.setOnClickListener(this);
+        aSwitch_add20.setOnClickListener(this);
+        aSwitch_add30.setOnClickListener(this);
+
+        button_clear_all_radios.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                radioButton_div2.setChecked(false);
+                radioButton_div4.setChecked(false);
+                radioButton_div8.setChecked(false);
+                calculateProgressBarValue();
+            }
+        });
+
+        editTextProgres.addTextChangedListener(new TextWatcher() {
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+            public void afterTextChanged(Editable s) {
+                int progressBarMax = Integer.parseInt(s.toString());
+                progressBar.setMax(progressBarMax);
+                preferencesHelper.setProgressBarMax(progressBarMax);
+            }
+        });
+
+        editTextSeekBar.addTextChangedListener(new TextWatcher() {
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+            public void afterTextChanged(Editable s) {
+                int seekBarMax = Integer.parseInt(s.toString());
+                seekBar.setMax(seekBarMax);
+                preferencesHelper.setSeekBarMax(seekBarMax);
+            }
+        });
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+            int progressChangedValue = 0;
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChangedValue = progress;
+            }
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                calculateProgressBarValue();
+                preferencesHelper.setProgressBarValue(progressChangedValue);
+                preferencesHelper.setSeekBarValue(progressChangedValue);
+            }
+        });
+
+        checkBoxToggles.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    toggleButton2x.setEnabled(true);
+                    toggleButton4x.setEnabled(true);
+                    toggleButton6x.setEnabled(true);
+                    toggleButton8x.setEnabled(true);
+                }else {
+                    toggleButton2x.setChecked(false);
+                    preferencesHelper.setToggle2xState(false);
+                    toggleButton2x.setEnabled(false);
+                    toggleButton4x.setChecked(false);
+                    preferencesHelper.setToggle4xState(false);
+                    toggleButton4x.setEnabled(false);
+                    toggleButton6x.setChecked(false);
+                    preferencesHelper.setToggle6xState(false);
+                    toggleButton6x.setEnabled(false);
+                    toggleButton8x.setChecked(false);
+                    preferencesHelper.setToggle8xState(false);
+                    toggleButton8x.setEnabled(false);
+                }
+                preferencesHelper.setCheckBoxState(isChecked);
+            }
+        });
+
+        calculateProgressBarValue();
+    }
+
+    private void calculateProgressBarValue(){
+        int progressBarValue;
+        progressBarValue = seekBar.getProgress();
+        progressBarValue = toggleButton2x.isChecked()? progressBarValue * 2: progressBarValue;
+        progressBarValue = toggleButton4x.isChecked()? progressBarValue * 4: progressBarValue;
+        progressBarValue = toggleButton6x.isChecked()? progressBarValue * 6: progressBarValue;
+        progressBarValue = toggleButton8x.isChecked()? progressBarValue * 8: progressBarValue;
+        progressBarValue = radioButton_div2.isChecked()? progressBarValue / 2: progressBarValue;
+        progressBarValue = radioButton_div4.isChecked()? progressBarValue / 4: progressBarValue;
+        progressBarValue = radioButton_div8.isChecked()? progressBarValue / 8: progressBarValue;
+        progressBarValue = aSwitch_add10.isChecked()? progressBarValue + 10: progressBarValue;
+        progressBarValue = aSwitch_add20.isChecked()? progressBarValue + 20: progressBarValue;
+        progressBarValue = aSwitch_add30.isChecked()? progressBarValue + 30: progressBarValue;
+        progressBar.setProgress(progressBarValue);
+        textViewProgress.setText("ProgressBar: " + String.valueOf(progressBar.getProgress()));
+        textViewSeek.setText("SeekBar: " + String.valueOf(seekBar.getProgress()));
+    }
+
+    @Override
+    public void onClick(View v) {
+        final PreferencesHelper preferencesHelper = PreferencesHelper.getInstance(this);
+        calculateProgressBarValue();
+        preferencesHelper.setProgressBarValue(progressBar.getProgress());
+        preferencesHelper.setSeekBarValue(seekBar.getProgress());
+        preferencesHelper.setToggle2xState(toggleButton2x.isChecked());
+        preferencesHelper.setToggle4xState(toggleButton4x.isChecked());
+        preferencesHelper.setToggle6xState(toggleButton6x.isChecked());
+        preferencesHelper.setToggle8xState(toggleButton8x.isChecked());
+        preferencesHelper.setRadioButtonDiv2State(radioButton_div2.isChecked());
+        preferencesHelper.setRadioButtonDiv4State(radioButton_div4.isChecked());
+        preferencesHelper.setRadioButtonDiv8State(radioButton_div8.isChecked());
+        preferencesHelper.setSwitchAdd10State(aSwitch_add10.isChecked());
+        preferencesHelper.setSwitchAdd20State(aSwitch_add20.isChecked());
+        preferencesHelper.setSwitchAdd30State(aSwitch_add30.isChecked());
     }
 }
--- app/src/main/java/com/example/hrant/dynamicprogressbar/PreferencesHelper.java
+++ app/src/main/java/com/example/hrant/dynamicprogressbar/PreferencesHelper.java
@@ -12,8 +12,21 @@ public class PreferencesHelper {
 
     private static final String PREFERENCES_NAME = "preferences";
 
-    private static final String LOGGED_IN = "logged_in";
-    private static final String USER_ID = "user_id";
+    private static final String PROGRESS_BAR_VALUE = "progress_bar_value";
+    private static final String PROGRESS_BAR_MAX = "progress_bar_max";
+    private static final String SEEK_BAR_VALUE = "seek_bar_value";
+    private static final String SEEK_BAR_MAX = "seek_bar_max";
+    private static final String CHEKBOX_STATE = "chekbox_state";
+    private static final String TOGGLE_BUTTON2x_STATE = "toggle_button2x_state";
+    private static final String TOGGLE_BUTTON4x_STATE = "toggle_button4x_state";
+    private static final String TOGGLE_BUTTON6x_STATE = "toggle_button6x_state";
+    private static final String TOGGLE_BUTTON8x_STATE = "toggle_button8x_state";
+    private static final String RADIO_BUTTON_DIV2_STATE = "toggle_button_div2_state";
+    private static final String RADIO_BUTTON_DIV4_STATE = "toggle_button_div4_state";
+    private static final String RADIO_BUTTON_DIV8_STATE = "toggle_button_div8_state";
+    private static final String SWITCH_ADD10 = "tswitch_add10";
+    private static final String SWITCH_ADD20 = "tswitch_add20";
+    private static final String SWITCH_ADD30 = "tswitch_add30";
 
     private SharedPreferences preferences;
 
@@ -31,38 +44,169 @@ public class PreferencesHelper {
         preferences = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE);
     }
 
-    public void resetAll() {
-        final SharedPreferences.Editor editor = getEditor();
-
-        editor.remove(LOGGED_IN);
-        editor.remove(USER_ID);
-
-        editor.apply();
-    }
-
     private SharedPreferences.Editor getEditor() {
         return preferences.edit();
     }
 
-    public boolean isLoggedIn() {
-        return preferences.getBoolean(LOGGED_IN, false);
+    public int getProgressBarValue() {
+        return preferences.getInt(PROGRESS_BAR_VALUE, 0);
     }
 
-    public void setLoggedIn(boolean loggedIn) {
+    public void setProgressBarValue(int value){
         final SharedPreferences.Editor editor = getEditor();
-
-        editor.putBoolean(LOGGED_IN, loggedIn);
+        editor.putInt(PROGRESS_BAR_VALUE, value);
         editor.apply();
     }
 
-    public long getUserId() {
-        return preferences.getLong(USER_ID, -1);
+    public int getProgressBarMax() {
+        return preferences.getInt(PROGRESS_BAR_MAX, 100);
     }
 
-    public void setUserId(long userId) {
+    public void setProgressBarMax(int value){
         final SharedPreferences.Editor editor = getEditor();
+        editor.putInt(PROGRESS_BAR_MAX, value);
+        getEditor().apply();
+    }
 
-        editor.putLong(USER_ID, userId);
+    public int getSeekBarValue() {
+        return preferences.getInt(SEEK_BAR_VALUE, 0);
+    }
+
+    public void setSeekBarValue(int value){
+        final SharedPreferences.Editor editor = getEditor();
+        editor.putInt(SEEK_BAR_VALUE, value);
+        getEditor().apply();
+    }
+
+    public int getSeekBarMax() {
+        return preferences.getInt(SEEK_BAR_MAX, 100);
+    }
+
+    public void setSeekBarMax(int value){
+        final SharedPreferences.Editor editor = getEditor();
+        editor.putInt(SEEK_BAR_MAX, value);
         editor.apply();
     }
+
+    public boolean getCheckBoxState() {
+        return preferences.getBoolean(CHEKBOX_STATE, false);
+    }
+
+    public void setCheckBoxState(boolean value){
+        final SharedPreferences.Editor editor = getEditor();
+        editor.putBoolean(CHEKBOX_STATE, value);
+        editor.apply();
+    }
+
+    public boolean getToggle2xState() {
+        return preferences.getBoolean(TOGGLE_BUTTON2x_STATE, false);
+    }
+
+    public void setToggle2xState(boolean value){
+        final SharedPreferences.Editor editor = getEditor();
+        editor.putBoolean(TOGGLE_BUTTON2x_STATE, value);
+        editor.apply();
+    }
+
+    public boolean getToggle4xState() {
+        return preferences.getBoolean(TOGGLE_BUTTON4x_STATE, false);
+    }
+
+    public void setToggle4xState(boolean value){
+        final SharedPreferences.Editor editor = getEditor();
+        editor.putBoolean(TOGGLE_BUTTON4x_STATE, value);
+        editor.apply();
+    }
+
+    public boolean getToggle6xState() {
+        return preferences.getBoolean(TOGGLE_BUTTON6x_STATE, false);
+    }
+
+    public void setToggle6xState(boolean value){
+        final SharedPreferences.Editor editor = getEditor();
+        editor.putBoolean(TOGGLE_BUTTON6x_STATE, value);
+        editor.apply();
+    }
+
+    public boolean getToggle8xState() {
+        return preferences.getBoolean(TOGGLE_BUTTON8x_STATE, false);
+    }
+
+    public void setToggle8xState(boolean value){
+        final SharedPreferences.Editor editor = getEditor();
+        editor.putBoolean(TOGGLE_BUTTON8x_STATE, value);
+        editor.apply();
+    }
+
+    public boolean getRadioButtonDiv2State() {
+        return preferences.getBoolean(RADIO_BUTTON_DIV2_STATE, false);
+    }
+
+    public void setRadioButtonDiv2State(boolean value){
+        final SharedPreferences.Editor editor = getEditor();
+        editor.putBoolean(RADIO_BUTTON_DIV2_STATE, value);
+        editor.apply();
+    }
+
+    public boolean getRadioButtonDiv4State() {
+        return preferences.getBoolean(RADIO_BUTTON_DIV4_STATE, false);
+    }
+
+    public void setRadioButtonDiv4State(boolean value){
+        final SharedPreferences.Editor editor = getEditor();
+        editor.putBoolean(RADIO_BUTTON_DIV4_STATE, value);
+        editor.apply();
+    }
+
+    public boolean getRadioButtonDiv8State() {
+        return preferences.getBoolean(RADIO_BUTTON_DIV8_STATE, false);
+    }
+
+    public void setRadioButtonDiv8State(boolean value){
+        final SharedPreferences.Editor editor = getEditor();
+        editor.putBoolean(RADIO_BUTTON_DIV8_STATE, value);
+        editor.apply();
+    }
+
+    public boolean getSwitchAdd10State() {
+        return preferences.getBoolean(SWITCH_ADD10, false);
+    }
+
+    public void setSwitchAdd10State(boolean value){
+        final SharedPreferences.Editor editor = getEditor();
+        editor.putBoolean(SWITCH_ADD10, value);
+        editor.apply();
+    }
+
+    public boolean getSwitchAdd20State() {
+        return preferences.getBoolean(SWITCH_ADD20, false);
+    }
+
+    public void setSwitchAdd20State(boolean value){
+        final SharedPreferences.Editor editor = getEditor();
+        editor.putBoolean(SWITCH_ADD20, value);
+        editor.apply();
+    }
+
+    public boolean getSwitchAdd30State() {
+        return preferences.getBoolean(SWITCH_ADD30, false);
+    }
+
+    public void setSwitchAdd30State(boolean value){
+        final SharedPreferences.Editor editor = getEditor();
+        editor.putBoolean(SWITCH_ADD30, value);
+        editor.apply();
+    }
+
+
+//    public long getUserId() {
+//        return preferences.getLong(USER_ID, -1);
+//    }
+//
+//    public void setUserId(long userId) {
+//        final SharedPreferences.Editor editor = getEditor();
+//
+//        editor.putLong(USER_ID, userId);
+//        editor.apply();
+//    }
 }
